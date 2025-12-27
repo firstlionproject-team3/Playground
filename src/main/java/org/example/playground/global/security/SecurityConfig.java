5fcf3c3bb91300992386c1fccfe6883fa5c46f72
kkhw1000 <kkhw1000@naver.com>
2025-12-27 16:47:52 +0900

    feat: passwordEncoder 추가 - PasswordEncoderFactories를 사용

--- src/main/java/org/example/playground/global/security/SecurityConfig.java
+++ src/main/java/org/example/playground/global/security/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -21,5 +24,9 @@ public class SecurityConfig {
         return http.build();
     }
 
-    //TODO: 패스워드 인코더 추가.
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
