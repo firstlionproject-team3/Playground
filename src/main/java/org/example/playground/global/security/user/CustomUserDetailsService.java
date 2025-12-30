e925199f8658edce8a1bfcc049ff9d5dd0db6371
jaehyun <wogusrla77@naver.com>
2025-12-30 09:15:40 +0900

    feat: CustomUserDetailsService 내부ì로직 주석해제 후 코드수정

--- src/main/java/org/example/playground/global/security/user/CustomUserDetailsService.java
+++ src/main/java/org/example/playground/global/security/user/CustomUserDetailsService.java
@@ -1,11 +1,16 @@
 package org.example.playground.global.security.user;
 
 import lombok.RequiredArgsConstructor;
+import org.example.playground.domain.user.dto.SecurityResponseForJWT;
 import org.example.playground.domain.user.service.UserService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 
 @Service
@@ -17,15 +22,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        // userService.~~~
-        // -> 여기서 userid, roles 가지고있는 dto 받아서 customUserDetails에 넣어서 반환
-
-        // Collection<? extends GrantedAuthority> roles = user.getRoles().stream()
-        //         .map(role -> new SimpleGrantedAuthority(role.getRole().getName()))
-        //         .toList();
-        //
-        // return new CustomUserDetails(user.getId(), roles);
-
-        return null;
+        SecurityResponseForJWT dto = userService.handleLogin(username);
+        Long id = dto.getId();
+        Collection<? extends GrantedAuthority> roles = dto.getRoles().stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+        return new CustomUserDetails(id, roles);
     }
 }
