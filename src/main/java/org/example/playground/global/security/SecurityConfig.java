cbde32d80e04096bf1a5a57559ce96c6e55ad9ab
jaehyun <wogusrla77@naver.com>
2026-01-02 18:36:03 +0900

    chore: cors 설정 PATCH 메서드 허용 추가

--- src/main/java/org/example/playground/global/security/SecurityConfig.java
+++ src/main/java/org/example/playground/global/security/SecurityConfig.java
@@ -98,7 +98,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000"
         ));
-        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
         config.setMaxAge(3600L);
