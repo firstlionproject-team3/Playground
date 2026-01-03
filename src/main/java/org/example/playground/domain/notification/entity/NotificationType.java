6d22be91bd560efabcd90e99c56c4d6c64f17aae
yeongbeomSong <s9091002@gmail.com>
2026-01-04 01:00:40 +0900

    feat: 추천/비추천 controller 추가 및 알림 유형 추가

--- src/main/java/org/example/playground/domain/notification/entity/NotificationType.java
+++ src/main/java/org/example/playground/domain/notification/entity/NotificationType.java
@@ -4,5 +4,7 @@ public enum NotificationType {
     ACCEPTED_ANSWER,   // 내 답변이 채택됨
     REPORT_RECEIVED,   // 신고 접수 (운영자 알림)
     NEW_ANSWER,        // 내 질문에 답변이 달림
-    NEW_COMMENT        // 내 게시물에 댓글이 달림
+    NEW_COMMENT,        // 내 게시물에 댓글이 달림
+    QUESTION_DELETED_BY_ADMIN,  // 관리자가 질문을 삭제
+    ANSWER_DELETED_BY_ADMIN     // 관리자가 답변을 삭제
 }
