6506827eaa159614203a2f4d42a6b43f6d6b201a
jaehyun <wogusrla77@naver.com>
2026-01-04 15:56:47 +0900

    chore: 에러 리다이렉트 허용

--- src/main/java/org/example/playground/global/config/SecurityConfig.java
+++ src/main/java/org/example/playground/global/config/SecurityConfig.java
@@ -65,6 +65,8 @@ public class SecurityConfig {
                         .requestMatchers("/oauth2/authorization/**","/login/oauth2/code/**").permitAll()
                         // h2 콘솔 - 테스트용
                         .requestMatchers("/h2-console/**").permitAll()
+                        // 에러 리다이렉트 허용
+                        .requestMatchers("/error").permitAll()
                         // 인증된 사용자만 접근 가능
                         // (Authorization 헤더에 유효한 accessToken 필요)
                         .anyRequest().authenticated())
