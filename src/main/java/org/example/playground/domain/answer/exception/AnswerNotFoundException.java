6476ac74e24ff72b1b6b3eaa2f1642cb0ea10e56
ysj20076 <msdh5301@gmail.com>
2026-01-04 19:21:52 +0900

    feat: 질문/답변 도메인 예외를 ErrorCode 기반으로 통합

--- src/main/java/org/example/playground/domain/answer/exception/AnswerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.playground.domain.answer.exception;
-
-public class AnswerNotFoundException extends RuntimeException{
-    public AnswerNotFoundException(Long answerId){
-        super("해당하는 답변을 찾을 수 없습니다 answerId = " + answerId);
-    }
-}
