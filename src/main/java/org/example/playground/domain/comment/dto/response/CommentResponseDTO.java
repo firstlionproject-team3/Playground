2c386bf4f5a728903b4bbd7dfb681ebcf136a4ed
jaehyun <wogusrla77@naver.com>
2026-01-05 14:24:17 +0900

    feat: Question 상세 조회시 응답DTO 추가

--- src/main/java/org/example/playground/domain/comment/dto/response/CommentResponseDTO.java
+++ src/main/java/org/example/playground/domain/comment/dto/response/CommentResponseDTO.java
@@ -28,7 +28,7 @@ public class CommentResponseDTO {
     public static CommentResponseDTO from(Comment comment) {
         return CommentResponseDTO.builder()
                 .id(comment.getId())
-                .nickname(comment.getUser().getNickname())
+                .nickname(comment.getUser().getNickname()) // 댓글 개수만큼 쿼리
                 .content(comment.getContent())
                 .likeCount(comment.getLikeCount())
                 .dislikeCount(comment.getDislikeCount())
