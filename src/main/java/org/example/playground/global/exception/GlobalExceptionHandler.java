88e54e3d5d12709a75c3b254a185ef97bf75677c
BokSIL-SEOB <yangminseob@gmail.com>
2026-01-05 15:04:02 +0900

    feat: GlobalExceptionHandler 내부 import 수정22

--- src/main/java/org/example/playground/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/playground/global/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
